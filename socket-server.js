952141fd42ae26b6a3299fea39c8a63cf91f3572
sanoyo <sa00380304yo@gmail.com>
2020-08-22 21:07:18 +0900

    ライブコーディング

--- socket-server.js
+++ socket-server.js
@@ -1,12 +1,21 @@
 'use strict'
 
 const socketIO = require('socket.io');
+const ot = require('ot');
+const roomList = {};
 
 module.exports = function(server) {
+    const str = 'This is a Markdown heading \n\n';
     const io = socketIO(server);
+
     io.on('connection', function(socket) {
+        const socketIOServer = new ot.EditorSocketIOServer(str, [], '', function(socket, cb) {
+            cb(true);
+            // roomList[data.room] = socketIOServer;
+        });
+
         socket.on('chatMessage', function(data) {
             io.emit('chatMessage', data);
-        })
+        }) 
     })
 }
