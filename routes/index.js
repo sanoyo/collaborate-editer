1a28a38be16d1ea70382e8c9e0f2367c72a3cc13
sanoyo <sa00380304yo@gmail.com>
2020-08-22 12:34:25 +0900

    contact form

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,28 @@ router.get('/', function(req, res, next) {
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Code4Share - a platform for sharing code.'});
 });
+
+router.route('/contact')
+  .get(function(req, res, next) {
+    res.render('contact', { title: 'Code4Share - a platform for sharing code.'});
+  })
+  .post(function(req, res, next) {
+    req.checkBody('name', 'Empty Name').notEmpty();
+    req.checkBody('email', 'Invalid Email').isEmail();
+    req.checkBody('message', 'Empty Message').notEmpty();
+    var errors = validationResult(req);
+
+    if(errors) {
+      res.render('contact', {
+        title: 'Code4Share - a platform for sharing code.',
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+        errorMessages: errors
+      });
+    } else {
+      res.render('thank', { title: 'Code4Share - a platform for sharing code.'});
+    }
+  })
 
 module.exports = router;
