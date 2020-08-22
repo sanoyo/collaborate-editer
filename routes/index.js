3e29582fb199d324ea187c14f424835a72b1739c
sanoyo <sa00380304yo@gmail.com>
2020-08-22 11:29:27 +0900

    スタイル変更

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Code4Share - a platform for sharing code.' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Code4Share - a platform for sharing code.'});
 });
 
 module.exports = router;
